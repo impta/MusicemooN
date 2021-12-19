b03eb9463e4bd789fdebe6fc95f099175012ae4d
impta <87300042+impta@users.noreply.github.com>
2021-12-20 00:32:39 +0330

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -224,8 +224,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/GapemooN'),
+                InlineKeyboardButton('🧩 Source', url='https://github.com/impta/MusicemooN'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
